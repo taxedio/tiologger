6d4be9c635ed768003e52bbe96d12ce6d3aeb695
Peter Donald <peterdonald3@outlook.com>
2022-03-31 18:03:19 +0100

    changed structure to be cleaner

--- logger.go
+++ logger.go
@@ -1,121 +1,163 @@
 package tiologger
 
 import (
+	"fmt"
 	"os"
+	"strings"
 
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
 
-var (
-	log      *zap.Logger
-	logLevel zap.AtomicLevel
-	err      error
+const (
+	envLogLevel  = "LOG_LEVEL"
+	envLogOutput = "LOG_OUTPUT"
 )
+
+var (
+	log logger
+)
+
+type tioLogger interface {
+	Print(v ...interface{})
+	Printf(format string, v ...interface{})
+}
+
+type logger struct {
+	log *zap.Logger
+}
 
 // init, initialises zap logger
 func init() {
-	logLevelVal := os.Getenv("LOG_LEVEL")
-	switch logLevelVal {
-	case "-1":
-		logLevel = zap.NewAtomicLevelAt(zap.DebugLevel)
-	case "0":
-		logLevel = zap.NewAtomicLevelAt(zap.InfoLevel)
-	case "1":
-		logLevel = zap.NewAtomicLevelAt(zap.WarnLevel)
-	case "2":
-		logLevel = zap.NewAtomicLevelAt(zap.ErrorLevel)
-	case "3":
-		logLevel = zap.NewAtomicLevelAt(zap.DPanicLevel)
-	case "4":
-		logLevel = zap.NewAtomicLevelAt(zap.PanicLevel)
-	case "5":
-		logLevel = zap.NewAtomicLevelAt(zap.FatalLevel)
-	default:
-		logLevel = zap.NewAtomicLevelAt(zap.ErrorLevel)
-	}
 	logConfig := zap.Config{
-		OutputPaths: []string{"stdout"},
-		Level:       logLevel,
+		OutputPaths: []string{getOutput()},
+		Level:       zap.NewAtomicLevelAt(getLevel()),
 		Encoding:    "json",
 		EncoderConfig: zapcore.EncoderConfig{
 			LevelKey:     "level",
 			TimeKey:      "time",
 			MessageKey:   "msg",
 			EncodeTime:   zapcore.ISO8601TimeEncoder,
-			EncodeLevel:  zapcore.LowercaseColorLevelEncoder,
+			EncodeLevel:  zapcore.LowercaseLevelEncoder,
 			EncodeCaller: zapcore.ShortCallerEncoder,
 		},
 	}
-	if log, err = logConfig.Build(); err != nil {
+
+	var err error
+	if log.log, err = logConfig.Build(); err != nil {
 		panic(err)
 	}
 }
 
+func getLevel() zapcore.Level {
+	switch strings.ToLower(strings.TrimSpace(os.Getenv(envLogLevel))) {
+	case "-1":
+		return zap.DebugLevel
+	case "0":
+		return zap.InfoLevel
+	case "1":
+		return zap.WarnLevel
+	case "2":
+		return zap.ErrorLevel
+	case "3":
+		return zap.DPanicLevel
+	case "4":
+		return zap.PanicLevel
+	case "5":
+		return zap.FatalLevel
+	default:
+		return zap.ErrorLevel
+	}
+}
+
+func getOutput() string {
+	output := strings.TrimSpace(os.Getenv(envLogOutput))
+	if output == "" {
+		return "stdout"
+	}
+	return output
+}
+
 // GetLogger, returns *zap.Logger
-func GetLogger() *zap.Logger {
+func GetLogger() tioLogger {
 	return log
+}
+
+func (l logger) Printf(format string, v ...interface{}) {
+	if len(v) == 0 {
+		Info(format)
+	} else {
+		Info(fmt.Sprintf(format, v...))
+	}
+}
+
+func (l logger) Print(v ...interface{}) {
+	Info(fmt.Sprintf("%v", v))
 }
 
 // Debug, creates log entry under "debug". Requires string
 func Debug(msg string, tags ...zap.Field) {
-	log.Info(msg, tags...)
-	if err := log.Sync(); err != nil {
-		log.Error(err.Error(), tags...)
+	log.log.Info(msg, tags...)
+	if err := log.log.Sync(); err != nil {
+		err = nil // TODO: update when https://github.com/uber-go/zap/issues/1000 is fixed
+		// log.log.Error("Debug sync error:  "+err.Error(), tags...)
 	}
 }
 
 // Info, creates log entry under "info". Requires string
 func Info(msg string, tags ...zap.Field) {
-	log.Info(msg, tags...)
-	if err := log.Sync(); err != nil {
-		log.Error(err.Error(), tags...)
+	log.log.Info(msg, tags...)
+	if err := log.log.Sync(); err != nil {
+		err = nil // TODO: update when https://github.com/uber-go/zap/issues/1000 is fixed
+		// log.log.Error("Info sync error:  "+err.Error(), tags...)
 	}
 }
 
 // Info, creates log entry under "info". Requires string
 func Warn(msg string, tags ...zap.Field) {
-	log.Warn(msg, tags...)
-	if err := log.Sync(); err != nil {
-		log.Error(err.Error(), tags...)
+	log.log.Warn(msg, tags...)
+	if err := log.log.Sync(); err != nil {
+		err = nil // TODO: update when https://github.com/uber-go/zap/issues/1000 is fixed
+		// log.log.Error("Warn sync error:  "+err.Error(), tags...)
 	}
 }
 
 // Error, creates log entry under "ERROR". Requires string and error
 func Error(msg string, err error, tags ...zap.Field) {
 	tags = append(tags, zap.NamedError("ERROR", err))
-	log.Error(msg, tags...)
-	if err := log.Sync(); err != nil {
-		log.Error(err.Error(), tags...)
-
+	log.log.Error(msg, tags...)
+	if err := log.log.Sync(); err != nil {
+		err = nil // TODO: update when https://github.com/uber-go/zap/issues/1000 is fixed
+		// log.log.Error("Error sync error:  "+err.Error(), tags...)
 	}
 }
 
 // Critical, creates log entry under "CRITICAL". Requires string and error
 func Critical(msg string, err error, tags ...zap.Field) {
 	tags = append(tags, zap.NamedError("CRITICAL", err))
-	log.Error(msg, tags...)
-	if err := log.Sync(); err != nil {
-		log.Error(err.Error(), tags...)
+	log.log.Error(msg, tags...)
+	if err := log.log.Sync(); err != nil {
+		err = nil // TODO: update when https://github.com/uber-go/zap/issues/1000 is fixed
+		// log.log.Error("Critical sync error:  "+err.Error(), tags...)
 	}
 }
 
 // DPanic, creates log entry under "D-PANIC". Requires string and error
 func DPanic(msg string, err error, tags ...zap.Field) {
 	tags = append(tags, zap.NamedError("D-PANIC", err))
-	log.DPanic(msg, tags...)
-	if err := log.Sync(); err != nil {
-		log.Error(err.Error(), tags...)
-
+	log.log.DPanic(msg, tags...)
+	if err := log.log.Sync(); err != nil {
+		err = nil // TODO: update when https://github.com/uber-go/zap/issues/1000 is fixed
+		// log.log.Error("DPanic sync error:  "+err.Error(), tags...)
 	}
 }
 
 // Panic, creates log entry under "PANIC". Requires string and error
 func Panic(msg string, err error, tags ...zap.Field) {
 	tags = append(tags, zap.NamedError("PANIC", err))
-	log.Panic(msg, tags...)
-	if err := log.Sync(); err != nil {
-		log.Error(err.Error(), tags...)
-
+	log.log.Panic(msg, tags...)
+	if err := log.log.Sync(); err != nil {
+		err = nil // TODO: update when https://github.com/uber-go/zap/issues/1000 is fixed
+		// log.log.Error("Panic sync error:  "+err.Error(), tags...)
 	}
 }
